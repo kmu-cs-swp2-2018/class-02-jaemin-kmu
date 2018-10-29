8648eeb70c8449d8eca1c0e7154f73cee7c44f36
jaemin-kmu <khn21c@kookmin.ac.kr>
2018-10-29 20:51:03 +0900

    assignment

---
+++ assn6/calcFunctions4.py
@@ -0,0 +1,72 @@
+from math import factorial as fact
+
+def factorial(numStr):
+    try:
+        n = int(numStr)
+        r = str(fact(n))
+    except:
+        r = 'Error!'
+    return r
+
+def decToBin(numStr):
+    try:
+        n = int(numStr)
+        r = bin(n)[2:]
+    except:
+        r = 'Error!'
+    return r
+
+def binToDec(numStr):
+    try:
+        n = int(numStr, 2)
+        r = str(n)
+    except:
+        r = 'Error!'
+    return r
+
+def decToRoman(numStr):
+    try:
+        n = int(numStr)
+    except:
+        return 'Error!'
+    
+    if n>= 4000:
+        return 'Error!'
+    
+    romans = [
+        (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+         (100, 'C'),  (90, 'XC'),  (50, 'L'),  (40, 'XL'),
+          (10, 'X'),   (9, 'IX'),   (5, 'V'),   (4, 'IV'),
+           (1, 'I')
+    ]
+
+    result = ''
+    for value, letters in romans:
+        while n >= value:
+            result += letters
+            n -= value
+    
+    return result
+
+def romanToDec(numStr):
+    sum = 0
+
+    romans = [
+        (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+        (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+        (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'),
+        (1, 'I')
+    ]
+
+    for value, letters in romans:
+        while numStr.find(letters) == 0:
+            numStr = numStr[len(letters):]
+            sum += value
+
+        if numStr == '':
+            result = sum
+        else:
+            result = 'Error!'
+
+    return result
+
