fa957b1c7a16f4e0cf0a5b9d2a5649efae057d45
jaemin-kmu <khn21c@kookmin.ac.kr>
2018-11-08 11:31:20 +0900

    assignment

--- assn7/game.py
+++ assn7/game.py
@@ -5,36 +5,52 @@ from word import Word
 
 def gameMain():
     word = Word('words.txt')
+    #guess 는 Guess 클래스에 랜덤으로 단어 하나를 인자로 던진 것
     guess = Guess(word.randFromDB())
 
     finished = False
     hangman = Hangman()
+    #목숨의 개수를 초기화
     maxTries = hangman.getLife()
-
+    #실패한 횟수가 목숨보다 적으면 계속 실행
     while guess.numTries < maxTries:
-
+        #display라는 변수를 통해 만들어서 행맨 모양을 불러옴
         display = hangman.get(maxTries - guess.numTries)
+        #행맨 모양 출력
         print(display)
+        #Current 와 Tries 출력
         guess.display()
-
+        #입력한 문자를 guessedChar라고 함
         guessedChar = input('Select a letter: ')
+        #입력한 문자가 한글자가 아니라면
         if len(guessedChar) != 1:
             print('One character at a time!')
             continue
+        #입력한 문자가 대문자라면
+        if 65 <= ord(guessedChar) <= 90:
+            #소문자로 변경
+            guessedChar = chr(ord(guessedChar) + 32)
+        #입력한 문자가 이미 입력했던 문자라면
         if guessedChar in guess.guessedChars:
             print('You already guessed \"' + guessedChar + '\"')
             continue
-
+        #알파벳 이외의 다른 모든 경우의 수를 차단
+        if ord(guessedChar) <= 64 or 91 <= ord(guessedChar) <= 96 or 123 <= ord(guessedChar):
+            print('Please enter the alphabet!')
+            continue
+        #guess 모듈로 넘어가서 입력한 문자에 대해 처리
         finished = guess.guess(guessedChar)
+        #단어가 완성되었을 때 while 탈출
         if finished == True:
             break
 
     if finished == True:
         print('Success')
+    #단어가 완성되어서 탈출한게 아니라 목숨의 개수가 다달아서 False인 상태에서 while문을 빠져나온 경우
     else:
         print(hangman.get(0))
         print('word [' + guess.secretWord + ']')
-        print('guess [' + guess.currentStatus + ']')
+        print('guess [' + ''.join(guess.currentStatus) + ']')
         print('Fail')
 
 
--- assn7/guess.py
+++ assn7/guess.py
@@ -1,12 +1,41 @@
 class Guess:
-
+    #game.py에서 guess를 정의하면서 Guess 클래스에 던진 단어 하나를 "word"로 받았음
     def __init__(self, word):
-        pass
-
+        #랜덤으로 받은 단어를 저장(이하 변수들은 game.py에 미리 쓰여있던 변수 이름들을 그대로 가져온 것)
+        self.secretWord = word
+        #입력했던 문자들을 저장하는 리스트
+        self.guessedChars = []
+        #실패한 횟수
+        self.numTries = 0
+        #맞추기전 초기상태
+        self.currentStatus = list('_'*len(word))
+        #맞추기전 초기상태의 또다른 코드
+        #self.currentStatus = []
+        #for i in range(len(word)):
+        #    self.currentStatus.append('_')
+        self.guessedChars2 = []
 
     def display(self):
-        pass
-
-
+        print('Current : ' + ''.join(self.currentStatus))
+        print("Tries : " , self.numTries)
+    #guessedChar 즉, 입력된 문자를 character로 받음
     def guess(self, character):
-        pass
+        #입력한 문자를 저장
+        self.guessedChars.append(character)
+        #입력한 문자가 단어에 있으면
+        if character in self.secretWord:
+            #단어에서 입력한 문자의 자리를 찾아 "_"를 입력한 문자로 변경
+            for i in range(len(self.secretWord)):
+                if self.secretWord[i] == character:
+                    self.currentStatus[i] = character
+        #입력한 문자가 단어에 없다면
+        else:
+            #실패한 횟수를 1회 추가
+            self.numTries += 1
+        #랜덤으로 받은 단어를 완벽하게 찾았다면
+        if self.secretWord == ''.join(self.currentStatus):
+            return True
+        #완벽하게 단어를 찾지 못했다면
+        #초기 값이 False였기 때문에 이 else문은 없어도 되긴함
+        else:
+            return False
